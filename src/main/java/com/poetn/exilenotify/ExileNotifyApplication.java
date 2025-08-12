1c28f6327d4bbc527b952817f5ca83234d942582
MarcosSMZ94 <marcos.smz76@gmail.com>
2025-08-12 15:52:40 -0300

    First

---
+++ src/main/java/com/poetn/exilenotify/ExileNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.poetn.exilenotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExileNotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExileNotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poetn/exilenotify/ExileNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poetn.exilenotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExileNotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
